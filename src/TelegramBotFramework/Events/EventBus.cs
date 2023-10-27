b460e8aef081d100deda65ce353e30c70f18c93d
Vladyslav Zaiets <rutova2@gmail.com>
2023-10-27 19:30:15 +0300

    refactor(configuration): polish DependencyInjectionSetup

---
+++ src/TelegramBotFramework/Events/EventBus.cs
@@ -0,0 +1,155 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Events;
+
+using System.Collections.Concurrent;
+
+/// <summary>
+/// In-process publish-subscribe event bus implementation.
+/// Manages event subscriptions and broadcasts events to all registered handlers.
+/// Thread-safe for concurrent operations.
+/// </summary>
+public class EventBus : IEventBus
+{
+    private readonly ConcurrentDictionary<Type, List<object>> _subscribers = new();
+    private readonly ILogger<EventBus> _logger;
+    private readonly object _syncLock = new();
+
+    public EventBus(ILogger<EventBus>? logger = null)
+    {
+        _logger = logger ?? new ConsoleLogger<EventBus>();
+    }
+
+    public void Subscribe<TEvent>(IEventHandler<TEvent> handler) where TEvent : class, IEvent
+    {
+        if (handler == null)
+            throw new ArgumentNullException(nameof(handler));
+
+        var eventType = typeof(TEvent);
+        var handlers = _subscribers.GetOrAdd(eventType, _ => new List<object>());
+
+        lock (_syncLock)
+        {
+            handlers.Add(handler);
+        }
+
+        _logger.LogInformation("Handler {HandlerName} subscribed to {EventType}",
+            handler.GetHandlerName(), eventType.Name);
+    }
+
+    public void Unsubscribe<TEvent>(IEventHandler<TEvent> handler) where TEvent : class, IEvent
+    {
+        if (handler == null)
+            return;
+
+        var eventType = typeof(TEvent);
+
+        if (_subscribers.TryGetValue(eventType, out var handlers))
+        {
+            lock (_syncLock)
+            {
+                handlers.Remove(handler);
+            }
+
+            _logger.LogInformation("Handler {HandlerName} unsubscribed from {EventType}",
+                handler.GetHandlerName(), eventType.Name);
+        }
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent @event) where TEvent : class, IEvent
+    {
+        if (@event == null)
+            throw new ArgumentNullException(nameof(@event));
+
+        var eventType = typeof(TEvent);
+
+        _logger.LogInformation("Publishing event {EventType} with ID {CorrelationId}",
+            eventType.Name, @event.CorrelationId);
+
+        if (!_subscribers.TryGetValue(eventType, out var handlers) || handlers.Count == 0)
+        {
+            _logger.LogWarning("No subscribers for event {EventType}", eventType.Name);
+            return;
+        }
+
+        // Create a copy of handlers list to avoid modification during iteration
+        List<object> handlersCopy;
+        lock (_syncLock)
+        {
+            handlersCopy = new List<object>(handlers);
+        }
+
+        var tasks = new List<Task>();
+
+        foreach (var handler in handlersCopy)
+        {
+            // Use reflection to call the handler
+            var handleMethod = handler.GetType()
+                .GetMethod("HandleAsync", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+
+            if (handleMethod != null)
+            {
+                try
+                {
+                    var task = (Task)handleMethod.Invoke(handler, new object[] { @event })!;
+                    tasks.Add(task);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error invoking handler for event {EventType}",
+                        eventType.Name);
+                }
+            }
+        }
+
+        // Wait for all handlers to complete
+        try
+        {
+            await Task.WhenAll(tasks);
+            _logger.LogInformation("Event {EventType} published to {Count} handlers successfully",
+                eventType.Name, handlersCopy.Count);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "One or more event handlers failed for {EventType}",
+                eventType.Name);
+            throw;
+        }
+    }
+
+    public void Clear()
+    {
+        lock (_syncLock)
+        {
+            _subscribers.Clear();
+        }
+
+        _logger.LogInformation("Event bus cleared, all subscriptions removed");
+    }
+
+    public int GetSubscriberCount<TEvent>() where TEvent : class, IEvent
+    {
+        var eventType = typeof(TEvent);
+
+        if (_subscribers.TryGetValue(eventType, out var handlers))
+        {
+            lock (_syncLock)
+            {
+                return handlers.Count;
+            }
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// Gets all registered event types.
+    /// </summary>
+    public IEnumerable<Type> GetRegisteredEventTypes()
+    {
+        return _subscribers.Keys;
+    }
+}
---
+++ src/TelegramBotFramework/Events/EventPublisher.cs
@@ -0,0 +1,104 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Events;
+
+/// <summary>
+/// Helper class for publishing events to the event bus.
+/// Provides convenience methods and ensures consistent event publishing.
+/// </summary>
+public class EventPublisher
+{
+    private readonly IEventBus _eventBus;
+    private readonly ILogger<EventPublisher> _logger;
+    private string? _correlationId;
+
+    public EventPublisher(IEventBus eventBus, ILogger<EventPublisher>? logger = null)
+    {
+        _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        _logger = logger ?? new ConsoleLogger<EventPublisher>();
+    }
+
+    /// <summary>
+    /// Sets the correlation ID for tracking related events.
+    /// </summary>
+    public EventPublisher WithCorrelationId(string correlationId)
+    {
+        _correlationId = correlationId;
+        return this;
+    }
+
+    /// <summary>
+    /// Publishes a message received event.
+    /// </summary>
+    public async Task PublishMessageReceivedAsync(long chatId, long userId, string? messageText)
+    {
+        var @event = new MessageReceivedEvent(chatId, userId, messageText, _correlationId);
+        await _eventBus.PublishAsync(@event);
+    }
+
+    /// <summary>
+    /// Publishes a command executed event.
+    /// </summary>
+    public async Task PublishCommandExecutedAsync(string commandName, long userId, string? arguments, bool success, string? errorMessage = null)
+    {
+        var @event = new CommandExecutedEvent(commandName, userId, arguments, success, errorMessage, _correlationId);
+        await _eventBus.PublishAsync(@event);
+    }
+
+    /// <summary>
+    /// Publishes a bot state changed event.
+    /// </summary>
+    public async Task PublishBotStateChangedAsync(string previousState, string newState, string? reason = null)
+    {
+        var @event = new BotStateChangedEvent(previousState, newState, reason, _correlationId);
+        await _eventBus.PublishAsync(@event);
+    }
+
+    /// <summary>
+    /// Publishes a custom event.
+    /// </summary>
+    public async Task PublishAsync<TEvent>(TEvent @event) where TEvent : class, IEvent
+    {
+        await _eventBus.PublishAsync(@event);
+    }
+}
+
+/// <summary>
+/// Example event handler for message received events.
+/// </summary>
+public class LoggingMessageEventHandler : EventHandlerBase<MessageReceivedEvent>
+{
+    public LoggingMessageEventHandler(ILogger<LoggingMessageEventHandler>? logger = null) : base(logger) { }
+
+    protected override Task ExecuteAsync(MessageReceivedEvent @event)
+    {
+        _logger.LogInformation("Message received from user {UserId} in chat {ChatId}: {Message}",
+            @event.UserId, @event.ChatId, @event.MessageText);
+
+        return Task.CompletedTask;
+    }
+}
+
+/// <summary>
+/// Example event handler for command executed events.
+/// </summary>
+public class LoggingCommandEventHandler : EventHandlerBase<CommandExecutedEvent>
+{
+    public LoggingCommandEventHandler(ILogger<LoggingCommandEventHandler>? logger = null) : base(logger) { }
+
+    protected override Task ExecuteAsync(CommandExecutedEvent @event)
+    {
+        var status = @event.Success ? "succeeded" : "failed";
+        var message = $"Command {status}: /{@event.CommandName} by user {@event.UserId}";
+
+        if (@event.ErrorMessage != null)
+            message += $" - Error: {@event.ErrorMessage}";
+
+        _logger.LogInformation(message);
+
+        return Task.CompletedTask;
+    }
+}
