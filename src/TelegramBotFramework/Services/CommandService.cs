b2a1f1dbc24e8cbd60baf48cc0057d723c2e7cc7
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-02 20:34:40 +0200

    readability pass over DependencyInjectionSetup.
    spotted a few of these, fixed them all here.

---
+++ src/TelegramBotFramework/Services/CommandService.cs
@@ -0,0 +1,170 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Services;
+
+/// <summary>
+/// Implementation of command management service.
+/// </summary>
+public class CommandService : ICommandService
+{
+    private readonly Repositories.ICommandRepository _commandRepository;
+    private readonly IUserService _userService;
+    private readonly Microsoft.Extensions.Logging.ILogger<CommandService> _logger;
+    private readonly Dictionary<string, int> _commandExecutionRateLimiter = new();
+    private readonly object _rateLimitLockObj = new();
+
+    public CommandService(
+        Repositories.ICommandRepository commandRepository,
+        IUserService userService,
+        Microsoft.Extensions.Logging.ILogger<CommandService> logger)
+    {
+        _commandRepository = commandRepository ?? throw new ArgumentNullException(nameof(commandRepository));
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<Models.Command?> GetCommandAsync(string commandName, CancellationToken cancellationToken = default)
+    {
+        var normalized = commandName.StartsWith("/") ? commandName : $"/{commandName}";
+        return await _commandRepository.GetByNameAsync(normalized, cancellationToken);
+    }
+
+    public async Task<Models.Command> RegisterCommandAsync(Models.Command command, CancellationToken cancellationToken = default)
+    {
+        command.Validate();
+        var created = await _commandRepository.CreateAsync(command, cancellationToken);
+        _logger.LogInformation("Command registered: {CommandName}", command.Name);
+        return created;
+    }
+
+    public async Task<bool> UnregisterCommandAsync(string commandName, CancellationToken cancellationToken = default)
+    {
+        var normalized = commandName.StartsWith("/") ? commandName : $"/{commandName}";
+        var result = await _commandRepository.DeleteAsync(normalized, cancellationToken);
+        if (result)
+        {
+            _logger.LogInformation("Command unregistered: {CommandName}", normalized);
+        }
+        return result;
+    }
+
+    public async Task<IList<Models.Command>> GetAvailableCommandsAsync(
+        Models.UserRole userRole = Models.UserRole.User,
+        CancellationToken cancellationToken = default)
+    {
+        var allCommands = await _commandRepository.GetEnabledAsync(cancellationToken);
+        return allCommands
+            .Where(c => !c.RequiresAdmin || userRole >= Models.UserRole.Administrator)
+            .ToList();
+    }
+
+    public async Task<Models.ExecutionContext> ExecuteCommandAsync(
+        Models.ExecutionContext context,
+        CancellationToken cancellationToken = default)
+    {
+        if (!context.Validate())
+        {
+            return context;
+        }
+
+        if (context.Command == null)
+        {
+            context.AddError("Command not specified in context");
+            return context;
+        }
+
+        try
+        {
+            if (!context.Command.IsEnabled)
+            {
+                context.AddError($"Command {context.Command.Name} is disabled");
+                return context;
+            }
+
+            if (context.Command.RequiresAdmin && context.User?.Role < Models.UserRole.Administrator)
+            {
+                context.AddError("Insufficient permissions to execute this command");
+                return context;
+            }
+
+            context.Command.RecordExecution();
+            await _commandRepository.UpdateAsync(context.Command, cancellationToken);
+
+            context.SetState("executed", true);
+            context.SetState("execution_time_ms", context.GetDuration().TotalMilliseconds);
+            _logger.LogInformation("Command executed: {CommandName} for user {UserId}",
+                context.Command.Name, context.UserId);
+        }
+        catch (Exception ex)
+        {
+            context.AddError($"Command execution failed: {ex.Message}");
+            _logger.LogError(ex, "Command execution error: {CommandName}", context.Command.Name);
+        }
+
+        return context;
+    }
+
+    public async Task<bool> CanUserExecuteCommandAsync(long userId, string commandName, CancellationToken cancellationToken = default)
+    {
+        var user = await _userService.GetUserByIdAsync(userId, cancellationToken);
+        if (user == null || user.Status != Models.UserStatus.Active)
+        {
+            return false;
+        }
+
+        var command = await GetCommandAsync(commandName, cancellationToken);
+        if (command == null || !command.IsEnabled)
+        {
+            return false;
+        }
+
+        return command.CanExecuteBy(user.Role);
+    }
+
+    public async Task<bool> IsCommandRateLimitedAsync(long userId, string commandName, CancellationToken cancellationToken = default)
+    {
+        await Task.Delay(0, cancellationToken);
+        var command = await GetCommandAsync(commandName, cancellationToken);
+        if (command?.RateLimitPerMinute == null)
+        {
+            return false;
+        }
+
+        lock (_rateLimitLockObj)
+        {
+            var key = $"{userId}:{commandName}";
+            if (!_commandExecutionRateLimiter.TryGetValue(key, out var count))
+            {
+                _commandExecutionRateLimiter[key] = 1;
+                return false;
+            }
+
+            if (count >= command.RateLimitPerMinute)
+            {
+                return true;
+            }
+
+            _commandExecutionRateLimiter[key]++;
+            return false;
+        }
+    }
+
+    public async Task RecordCommandExecutionAsync(string commandName, CancellationToken cancellationToken = default)
+    {
+        var command = await GetCommandAsync(commandName, cancellationToken);
+        if (command != null)
+        {
+            command.RecordExecution();
+            await _commandRepository.UpdateAsync(command, cancellationToken);
+        }
+    }
+
+    public async Task<int> GetCommandExecutionCountAsync(string commandName, CancellationToken cancellationToken = default)
+    {
+        var command = await GetCommandAsync(commandName, cancellationToken);
+        return command?.ExecutionCount ?? 0;
+    }
+}
---
+++ src/TelegramBotFramework/Services/IUserService.cs
@@ -0,0 +1,130 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Services;
+
+/// <summary>
+/// Service interface for user management operations.
+/// </summary>
+public interface IUserService
+{
+    Task<Models.BotUser> GetOrCreateUserAsync(long telegramId, string firstName, string? lastName = null, CancellationToken cancellationToken = default);
+
+    Task<Models.BotUser?> GetUserByIdAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<Models.BotUser?> GetUserByTelegramIdAsync(long telegramId, CancellationToken cancellationToken = default);
+
+    Task<Models.BotUser> UpdateUserAsync(Models.BotUser user, CancellationToken cancellationToken = default);
+
+    Task<bool> BanUserAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<bool> UnbanUserAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<IList<Models.BotUser>> GetAdministratorsAsync(CancellationToken cancellationToken = default);
+
+    Task<bool> PromoteToAdminAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<bool> DemoteAdminAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<int> GetTotalUsersCountAsync(CancellationToken cancellationToken = default);
+
+    Task<int> GetActiveUsersCountAsync(CancellationToken cancellationToken = default);
+
+    Task RecordUserActivityAsync(long userId, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Service interface for command management and execution.
+/// </summary>
+public interface ICommandService
+{
+    Task<Models.Command?> GetCommandAsync(string commandName, CancellationToken cancellationToken = default);
+
+    Task<Models.Command> RegisterCommandAsync(Models.Command command, CancellationToken cancellationToken = default);
+
+    Task<bool> UnregisterCommandAsync(string commandName, CancellationToken cancellationToken = default);
+
+    Task<IList<Models.Command>> GetAvailableCommandsAsync(Models.UserRole userRole = Models.UserRole.User, CancellationToken cancellationToken = default);
+
+    Task<Models.ExecutionContext> ExecuteCommandAsync(Models.ExecutionContext context, CancellationToken cancellationToken = default);
+
+    Task<bool> CanUserExecuteCommandAsync(long userId, string commandName, CancellationToken cancellationToken = default);
+
+    Task<bool> IsCommandRateLimitedAsync(long userId, string commandName, CancellationToken cancellationToken = default);
+
+    Task RecordCommandExecutionAsync(string commandName, CancellationToken cancellationToken = default);
+
+    Task<int> GetCommandExecutionCountAsync(string commandName, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Service interface for session management.
+/// </summary>
+public interface ISessionService
+{
+    Task<Models.UserSession> CreateSessionAsync(long userId, long chatId, CancellationToken cancellationToken = default);
+
+    Task<Models.UserSession?> GetActiveSessionAsync(long userId, CancellationToken cancellationToken = default);
+
+    Task<Models.UserSession?> GetSessionAsync(string sessionId, CancellationToken cancellationToken = default);
+
+    Task<bool> UpdateSessionContextAsync(string sessionId, string contextKey, string value, CancellationToken cancellationToken = default);
+
+    Task<string?> GetSessionContextAsync(string sessionId, string contextKey, CancellationToken cancellationToken = default);
+
+    Task<bool> CloseSessionAsync(string sessionId, CancellationToken cancellationToken = default);
+
+    Task<int> CloseExpiredSessionsAsync(CancellationToken cancellationToken = default);
+
+    Task<Models.UserSession> NavigateToMenuAsync(string sessionId, string menuId, CancellationToken cancellationToken = default);
+
+    Task RecordSessionActivityAsync(string sessionId, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Service interface for menu management and navigation.
+/// </summary>
+public interface IMenuService
+{
+    Task<Models.Menu?> GetMenuAsync(string menuId, CancellationToken cancellationToken = default);
+
+    Task<Models.Menu> CreateMenuAsync(Models.Menu menu, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteMenuAsync(string menuId, CancellationToken cancellationToken = default);
+
+    Task<Models.Menu> UpdateMenuAsync(Models.Menu menu, CancellationToken cancellationToken = default);
+
+    Task<Models.MenuButton?> GetButtonAsync(string menuId, string callbackData, CancellationToken cancellationToken = default);
+
+    Task<Models.Menu> AddButtonAsync(string menuId, Models.MenuButton button, CancellationToken cancellationToken = default);
+
+    Task<bool> RemoveButtonAsync(string menuId, string callbackData, CancellationToken cancellationToken = default);
+
+    Task<IList<Models.Menu>> GetActiveMenusAsync(CancellationToken cancellationToken = default);
+
+    Task<List<List<Models.MenuButton>>> GetArrangedButtonsAsync(string menuId, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Service interface for message processing.
+/// </summary>
+public interface IMessageService
+{
+    Task<Models.Message> ProcessIncomingMessageAsync(Models.Message message, CancellationToken cancellationToken = default);
+
+    Task<Models.Message?> GetMessageAsync(long messageId, CancellationToken cancellationToken = default);
+
+    Task<IList<Models.Message>> GetUserMessagesAsync(long userId, int limit = 50, CancellationToken cancellationToken = default);
+
+    Task<IList<Models.Message>> GetFailedMessagesAsync(int limit = 100, CancellationToken cancellationToken = default);
+
+    Task<bool> MarkAsProcessedAsync(long messageId, CancellationToken cancellationToken = default);
+
+    Task<bool> MarkAsFailedAsync(long messageId, string errorMessage, CancellationToken cancellationToken = default);
+
+    Task<int> GetUnprocessedMessageCountAsync(CancellationToken cancellationToken = default);
+
+    Task ArchiveOldMessagesAsync(int daysOld = 30, CancellationToken cancellationToken = default);
+}
