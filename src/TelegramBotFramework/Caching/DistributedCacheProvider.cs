c253c95263d7b6a9da6fdc3a10d03ba6587bce6d
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-03 08:58:20 +0200

    refactor(backgroundworkers): cleanup in BackgroundTaskWorker

---
+++ src/TelegramBotFramework/Caching/DistributedCacheProvider.cs
@@ -0,0 +1,179 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Caching;
+
+using System.Text.Json;
+
+/// <summary>
+/// Abstract base for distributed cache providers (Redis, Memcached, etc).
+/// Provides serialization/deserialization and common cache operations.
+/// Subclass this for specific distributed cache implementations.
+/// </summary>
+public abstract class DistributedCacheProvider : ICacheProvider
+{
+    protected readonly ILogger<DistributedCacheProvider> _logger;
+
+    protected DistributedCacheProvider(ILogger<DistributedCacheProvider>? logger = null)
+    {
+        _logger = logger ?? new ConsoleLogger<DistributedCacheProvider>();
+    }
+
+    public virtual async Task<T?> GetAsync<T>(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            return default;
+
+        try
+        {
+            var value = await GetValueAsync(key);
+            if (value == null)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(value);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting value from distributed cache: {Key}", key);
+            return default;
+        }
+    }
+
+    public virtual async Task SetAsync<T>(string key, T value, TimeSpan? expiration = null)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            return;
+
+        try
+        {
+            var json = JsonSerializer.Serialize(value);
+            await SetValueAsync(key, json, expiration);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error setting value in distributed cache: {Key}", key);
+        }
+    }
+
+    public virtual async Task RemoveAsync(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            return;
+
+        try
+        {
+            await RemoveValueAsync(key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error removing value from distributed cache: {Key}", key);
+        }
+    }
+
+    public virtual async Task<bool> ExistsAsync(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            return false;
+
+        try
+        {
+            return await KeyExistsAsync(key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error checking existence in distributed cache: {Key}", key);
+            return false;
+        }
+    }
+
+    public virtual async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiration = null)
+    {
+        var existing = await GetAsync<T>(key);
+        if (existing != null)
+            return existing;
+
+        var value = await factory();
+        await SetAsync(key, value, expiration);
+        return value;
+    }
+
+    public virtual async Task FlushAsync()
+    {
+        try
+        {
+            await FlushAllAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error flushing distributed cache");
+        }
+    }
+
+    public virtual async Task<CacheStatistics> GetStatisticsAsync()
+    {
+        try
+        {
+            return await GetStatsAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting distributed cache statistics");
+            return new CacheStatistics();
+        }
+    }
+
+    /// <summary>
+    /// Subclasses must implement this to get a value from the distributed cache.
+    /// </summary>
+    protected abstract Task<string?> GetValueAsync(string key);
+
+    /// <summary>
+    /// Subclasses must implement this to set a value in the distributed cache.
+    /// </summary>
+    protected abstract Task SetValueAsync(string key, string value, TimeSpan? expiration);
+
+    /// <summary>
+    /// Subclasses must implement this to remove a value from the distributed cache.
+    /// </summary>
+    protected abstract Task RemoveValueAsync(string key);
+
+    /// <summary>
+    /// Subclasses must implement this to check if a key exists in the distributed cache.
+    /// </summary>
+    protected abstract Task<bool> KeyExistsAsync(string key);
+
+    /// <summary>
+    /// Subclasses must implement this to flush all cache entries.
+    /// </summary>
+    protected abstract Task FlushAllAsync();
+
+    /// <summary>
+    /// Subclasses should override this to provide cache statistics.
+    /// </summary>
+    protected virtual Task<CacheStatistics> GetStatsAsync()
+    {
+        return Task.FromResult(new CacheStatistics());
+    }
+}
+
+/// <summary>
+/// No-operation distributed cache provider for testing/fallback scenarios.
+/// Useful as a fallback when distributed cache is unavailable.
+/// </summary>
+public class NoOpCacheProvider : ICacheProvider
+{
+    public Task<T?> GetAsync<T>(string key) => Task.FromResult<T?>(default);
+    public Task SetAsync<T>(string key, T value, TimeSpan? expiration = null) => Task.CompletedTask;
+    public Task RemoveAsync(string key) => Task.CompletedTask;
+    public Task<bool> ExistsAsync(string key) => Task.FromResult(false);
+
+    public async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiration = null)
+    {
+        return await factory();
+    }
+
+    public Task FlushAsync() => Task.CompletedTask;
+    public Task<CacheStatistics> GetStatisticsAsync() => Task.FromResult(new CacheStatistics());
+}
