a87b26a52ad0b6cf83209acfe3b86f700f56a29b
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-15 18:57:57 +0200

    refactor(configuration): tidy up DependencyInjectionSetup

---
+++ src/TelegramBotFramework/Integration/IWebhookService.cs
@@ -0,0 +1,63 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Integration;
+
+/// <summary>
+/// Manages the full lifecycle of webhook mode: registering the webhook URL with
+/// Telegram on startup, dispatching received updates to subscribed handlers, and
+/// removing the webhook on shutdown.
+/// </summary>
+public interface IWebhookService
+{
+    /// <summary>
+    /// Raised whenever a validated update arrives at the webhook endpoint.
+    /// Subscribe to this event to receive and process updates.
+    /// </summary>
+    event Func<TelegramUpdate, Task>? OnUpdateReceived;
+
+    /// <summary>
+    /// Registers the configured webhook URL with Telegram and makes the bot ready to
+    /// receive updates over HTTPS. Called automatically on application startup when
+    /// registered as a hosted service.
+    /// </summary>
+    /// <param name="cancellationToken">Propagates notification that the operation should be cancelled.</param>
+    Task RegisterAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Removes the webhook registration from Telegram, causing the bot to stop receiving
+    /// updates via push. Called automatically on application shutdown.
+    /// </summary>
+    /// <param name="cancellationToken">Propagates notification that the operation should be cancelled.</param>
+    Task UnregisterAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Dispatches an already-parsed update to all <see cref="OnUpdateReceived"/> subscribers.
+    /// Typically called by the webhook controller upon receiving a valid request from Telegram.
+    /// </summary>
+    /// <param name="update">The parsed update to dispatch.</param>
+    /// <param name="cancellationToken">Propagates notification that the operation should be cancelled.</param>
+    Task DispatchUpdateAsync(TelegramUpdate update, CancellationToken cancellationToken = default);
+
+    /// <summary>Gets current runtime info about the webhook.</summary>
+    WebhookInfo GetInfo();
+}
+
+/// <summary>Runtime information about the current webhook state.</summary>
+public sealed class WebhookInfo
+{
+    /// <summary>Gets a value indicating whether the webhook is currently registered.</summary>
+    public bool IsRegistered { get; init; }
+
+    /// <summary>Gets the registered webhook URL, or <c>null</c> when not registered.</summary>
+    public string? Url { get; init; }
+
+    /// <summary>Gets the UTC timestamp of the last successful registration, or <c>null</c>.</summary>
+    public DateTime? RegisteredAt { get; init; }
+
+    /// <summary>Gets the number of updates dispatched since the last registration.</summary>
+    public long UpdatesDispatched { get; init; }
+}
---
+++ src/TelegramBotFramework/Integration/WebhookOptions.cs
@@ -0,0 +1,69 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Integration;
+
+/// <summary>
+/// Configuration options for webhook mode.
+/// Pass an instance to <c>services.AddWebhookMode(options)</c> or configure
+/// via <see cref="BotConfiguration.WebhookUrl"/> and <see cref="BotConfiguration.WebhookSecret"/>.
+/// </summary>
+public sealed class WebhookOptions
+{
+    /// <summary>
+    /// Gets or sets the HTTPS URL Telegram will send updates to.
+    /// Must be reachable from the public internet on port 443, 80, 88, or 8443.
+    /// </summary>
+    public string Url { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets an optional secret token (1–256 ASCII characters) that Telegram
+    /// includes in the <c>X-Telegram-Bot-Api-Secret-Token</c> header of every request.
+    /// Use this to verify that requests genuinely originate from Telegram.
+    /// </summary>
+    public string? SecretToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum number of simultaneous HTTPS connections Telegram
+    /// may use for delivering updates. Accepted values: 1–100. Defaults to <c>40</c>.
+    /// </summary>
+    public int MaxConnections { get; set; } = 40;
+
+    /// <summary>
+    /// Gets or sets the list of update types the bot should receive.
+    /// Passing <c>null</c> or an empty array instructs Telegram to send all types.
+    /// </summary>
+    public string[]? AllowedUpdates { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relative path at which the webhook controller listens.
+    /// Defaults to <c>/api/webhook/telegram</c>.
+    /// </summary>
+    public string ListenPath { get; set; } = "/api/webhook/telegram";
+
+    /// <summary>
+    /// Gets or sets whether pending updates that accumulated while the webhook was
+    /// not configured should be discarded on registration. Defaults to <c>false</c>.
+    /// </summary>
+    public bool DropPendingUpdates { get; set; }
+
+    /// <summary>Validates required fields.</summary>
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Url))
+            throw new InvalidOperationException("WebhookOptions.Url must be set.");
+
+        if (!Uri.TryCreate(Url, UriKind.Absolute, out var uri) ||
+            (uri.Scheme != "https" && uri.Scheme != "http"))
+            throw new InvalidOperationException($"WebhookOptions.Url '{Url}' is not a valid absolute URL.");
+
+        if (MaxConnections is < 1 or > 100)
+            throw new InvalidOperationException("MaxConnections must be between 1 and 100.");
+
+        if (!string.IsNullOrEmpty(ListenPath) && !ListenPath.StartsWith('/'))
+            throw new InvalidOperationException("ListenPath must start with '/'.");
+    }
+}
