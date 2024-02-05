9f7cd0169160baf88efcff8b7178c6a021e93bcf
Vladyslav Zaiets <rutova2@gmail.com>
2024-02-05 12:01:07 +0200

    Extension methods on Message

--- src/TelegramBotFramework/Models/MessageExtensions.cs
+++ src/TelegramBotFramework/Models/MessageExtensions.cs
@@ -12,9 +12,11 @@ public static class MessageExtensions
     /// </summary>
     /// <param name="message">The message to check.</param>
     /// <returns>True if the message represents a command; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> is null.</exception>
     public static bool IsCommand(this Message message)
     {
-        return message.CommandName != null && message.CommandName.StartsWith('/');
+        ArgumentNullException.ThrowIfNull(message);
+        return message.CommandName is not null && message.CommandName.StartsWith('/');
     }
 
     /// <summary>
@@ -22,9 +24,11 @@ public static class MessageExtensions
     /// </summary>
     /// <param name="message">The message to check.</param>
     /// <returns>True if the message has attachments; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> is null.</exception>
     public static bool HasAttachments(this Message message)
     {
-        return message.AttachmentUrls != null && message.AttachmentUrls.Count > 0;
+        ArgumentNullException.ThrowIfNull(message);
+        return message.AttachmentUrls is not null && message.AttachmentUrls.Count > 0;
     }
 
     /// <summary>
@@ -32,8 +36,10 @@ public static class MessageExtensions
     /// </summary>
     /// <param name="message">The message to get type for.</param>
     /// <returns>Lowercase string representation of the message type.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> is null.</exception>
     public static string GetTypeString(this Message message)
     {
+        ArgumentNullException.ThrowIfNull(message);
         return message.Type.ToString().ToLowerInvariant();
     }
 
@@ -42,8 +48,10 @@ public static class MessageExtensions
     /// </summary>
     /// <param name="message">The message to check.</param>
     /// <returns>True if the message is a reply; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> is null.</exception>
     public static bool IsReply(this Message message)
     {
+        ArgumentNullException.ThrowIfNull(message);
         return message.ReplyToMessageId.HasValue && message.ReplyToMessageId > 0;
     }
 }
