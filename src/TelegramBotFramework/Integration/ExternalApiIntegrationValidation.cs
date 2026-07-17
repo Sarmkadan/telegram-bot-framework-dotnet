df81295cb0fb11f2cefd31d3a284fe3ed0f00775
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 12:47:27 +0300

    tidy up ExternalApiIntegrationValidation
    mostly ExternalApiIntegrationValidation.cs, nothing exciting.

--- src/TelegramBotFramework/Integration/ExternalApiIntegrationValidation.cs
+++ src/TelegramBotFramework/Integration/ExternalApiIntegrationValidation.cs
@@ -9,6 +9,7 @@ namespace TelegramBotFramework.Integration;
 
 /// <summary>
 /// Provides validation helpers for <see cref="ExternalApiIntegration"/> instances.
+/// Validates configuration, dependencies, and state of external API integrations.
 /// </summary>
 public static class ExternalApiIntegrationValidation
 {
@@ -30,18 +31,13 @@ public static class ExternalApiIntegrationValidation
     /// </summary>
     /// <param name="value">The instance to validate.</param>
     /// <returns><see langword="true"/> if valid; otherwise, <see langword="false"/>.</returns>
-    public static bool IsValid(this ExternalApiIntegration? value)
-    {
-        return value is not null;
-    }
+    public static bool IsValid(this ExternalApiIntegration? value) => value is not null;
 
     /// <summary>
     /// Ensures that the specified <see cref="ExternalApiIntegration"/> instance is valid.
     /// </summary>
     /// <param name="value">The instance to validate.</param>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
-    public static void EnsureValid(this ExternalApiIntegration? value)
-    {
+    public static void EnsureValid(this ExternalApiIntegration? value) =>
         ArgumentNullException.ThrowIfNull(value);
-    }
 }
