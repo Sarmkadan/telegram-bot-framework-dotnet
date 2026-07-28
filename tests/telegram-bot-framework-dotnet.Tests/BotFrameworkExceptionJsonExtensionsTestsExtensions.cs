3c02079f6fae1e945b3dac6d247aa19ed6249a24
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 06:15:27 +0300

    BotFrameworkExceptionJsonExtensions extensions

---
+++ tests/telegram-bot-framework-dotnet.Tests/BotFrameworkExceptionJsonExtensionsTestsExtensions.cs
@@ -0,0 +1,4 @@
+public static class BotFrameworkExceptionJsonExtensionsTestsExtensions
+{
+    // Implement 2-4 genuinely useful extension methods for the existing type BotFrameworkExceptionJsonExtensionsTests
+}
