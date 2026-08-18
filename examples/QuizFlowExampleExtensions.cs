73c57d444e247f7d115ec8189662d1adb85cc8e6
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-18 14:28:26 +0000

    chore: apply workspace updates

---
+++ examples/QuizFlowExampleExtensions.cs
@@ -0,0 +1,22 @@
+public static class QuizFlowExampleExtensions
+{
+    public async Task RegisterSampleQuizAsync()
+    {
+        // implementation
+    }
+
+    public async Task RunQuizExampleAsync()
+    {
+        // implementation
+    }
+
+    public async Task CheckQuizStatusAsync()
+    {
+        // implementation
+    }
+
+    public async Task AbortQuizExampleAsync()
+    {
+        // implementation
+    }
+}
