78b1ea6638875e2ada6f9b8f5f660e5f74c9be4c
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-28 18:05:04 +0000

    chore: apply workspace updates

--- src/TelegramBotFramework/Models/TelegramBotFrameworkDotnetOptionsExtensionsConstants.cs
+++ src/TelegramBotFramework/Models/TelegramBotFrameworkDotnetOptionsExtensionsConstants.cs
@@ -2,5 +2,7 @@ namespace TelegramBotFramework.Models
 {
     internal static class TelegramBotFrameworkDotnetOptionsExtensionsConstants
     {
+        public const string BotTokenNullOrEmptyExceptionMessage = "BotToken cannot be null or empty.";
+        public const string BotUsernameNullOrEmptyExceptionMessage = "BotUsername cannot be null or empty.";
     }
 }
