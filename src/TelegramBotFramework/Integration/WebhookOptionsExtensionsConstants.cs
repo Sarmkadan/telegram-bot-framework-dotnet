acaa4341be16e3c159225a1e06b38ecc0afc6073
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 06:45:48 +0000

    feat(Integration): add WebhookOptionsExtensionsConstants class

---
+++ src/TelegramBotFramework/Integration/WebhookOptionsExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace TelegramBotFramework.Integration;
+
+/// <summary>
+/// Constants for webhook options extensions.
+/// </summary>
+internal static class WebhookOptionsExtensionsConstants
+{
+}
