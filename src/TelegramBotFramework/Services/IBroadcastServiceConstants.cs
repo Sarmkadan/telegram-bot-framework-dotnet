72d21e139b61bc39b6212d2e969133c81a15ff58
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 15:55:36 +0000

    feat(IBroadcastService): extract magic values to IBroadcastServiceConstants.cs

---
+++ src/TelegramBotFramework/Services/IBroadcastServiceConstants.cs
@@ -0,0 +1,14 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Services;
+
+/// <summary>
+/// Constants for the IBroadcastService.
+/// </summary>
+internal static class IBroadcastServiceConstants
+{
+}
