c2d873e9dd7f311c610aab5329e17c86e6bf9e05
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 06:49:20 +0000

    feat(Integration): add HttpClientFactoryJsonExtensionsConstants class

---
+++ src/TelegramBotFramework/Integration/HttpClientFactoryJsonExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace TelegramBotFramework.Integration;
+
+/// <summary>
+/// Constants for HttpClientFactory json extensions.
+/// </summary>
+internal static class HttpClientFactoryJsonExtensionsConstants
+{
+}
