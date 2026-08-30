177f274cb54ab82a3d698c668055428f99106972
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 16:36:13 +0000

    feat(IScheduledMessageService): extract magic values to IScheduledMessageServiceConstants.cs

---
+++ src/TelegramBotFramework/Services/IScheduledMessageServiceConstants.cs
@@ -0,0 +1,13 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// =============================================================================
+
+namespace TelegramBotFramework.Services;
+
+/// <summary>
+/// Constants for the IScheduledMessageService.
+/// </summary>
+internal static class IScheduledMessageServiceConstants
+{
+}
