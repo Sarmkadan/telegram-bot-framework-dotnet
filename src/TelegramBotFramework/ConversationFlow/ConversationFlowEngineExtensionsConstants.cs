c44d33f22ef833501791269c66daaf3cb77feb52
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 09:40:37 +0000

    feat(ConversationFlow): extract magic values to ConversationFlowEngineExtensionsConstants

---
+++ src/TelegramBotFramework/ConversationFlow/ConversationFlowEngineExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace TelegramBotFramework.ConversationFlow;
+
+/// <summary>
+/// Constants for ConversationFlowEngineExtensions.
+/// </summary>
+internal static class ConversationFlowEngineExtensionsConstants
+{
+}
