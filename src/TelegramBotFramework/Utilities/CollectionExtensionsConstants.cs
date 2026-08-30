e09da0c9ef71c718ac8ad150ae0879be522f5ad3
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 20:10:55 +0000

    feat(CollectionExtensions): extract magic values to CollectionExtensionsConstants.cs

---
+++ src/TelegramBotFramework/Utilities/CollectionExtensionsConstants.cs
@@ -0,0 +1,10 @@
+#nullable enable
+
+namespace TelegramBotFramework.Utilities;
+
+/// <summary>
+/// Constants for CollectionExtensions.
+/// </summary>
+internal static class CollectionExtensionsConstants
+{
+}
