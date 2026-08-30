aa39ee2593ec2d7269e3826f047cfae100a6c4f1
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-30 19:58:35 +0000

    feat(Caching): extract magic values to LocalCacheProviderExtensionsConstants.cs

---
+++ src/TelegramBotFramework/Caching/LocalCacheProviderExtensionsConstants.cs
@@ -0,0 +1,14 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace TelegramBotFramework.Caching;
+
+/// <summary>
+/// Constants for <see cref="LocalCacheProviderExtensions"/>.
+/// </summary>
+internal static class LocalCacheProviderExtensionsConstants
+{
+}
