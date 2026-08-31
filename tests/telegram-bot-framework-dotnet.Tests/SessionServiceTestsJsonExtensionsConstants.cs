e5bf6a0aaef9acf1498b8c01c58253a8470cb9e1
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-31 13:35:21 +0000

    refactor(Tests): extract magic values to SessionServiceTestsJsonExtensionsConstants

--- tests/telegram-bot-framework-dotnet.Tests/SessionServiceTestsJsonExtensionsConstants.cs
+++ tests/telegram-bot-framework-dotnet.Tests/SessionServiceTestsJsonExtensionsConstants.cs
@@ -2,17 +2,9 @@
 
 using System.Text.Json;
 
-namespace TelegramBotFramework.Tests
+namespace TelegramBotFramework.Tests;
+
+internal static class SessionServiceTestsJsonExtensionsConstants
 {
-    /// <summary>
-    /// Provides shared values used by <see cref="SessionServiceTestsJsonExtensions"/>.
-    /// </summary>
-    internal static class SessionServiceTestsJsonExtensionsConstants
-    {
-        public static readonly JsonSerializerOptions BaseJsonSerializerOptions = new(JsonSerializerDefaults.Web)
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            WriteIndented = false
-        };
-    }
+    public static readonly JsonSerializerOptions BaseJsonSerializerOptions = new();
 }
