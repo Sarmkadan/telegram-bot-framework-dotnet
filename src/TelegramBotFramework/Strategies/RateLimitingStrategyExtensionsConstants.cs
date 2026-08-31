482ae58b5d9786bdc50a9ee3b712c1865a4d6ab9
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-31 04:00:16 +0000

    refactor(Strategies): add RateLimitingStrategyExtensionsConstants

---
+++ src/TelegramBotFramework/Strategies/RateLimitingStrategyExtensionsConstants.cs
@@ -0,0 +1,8 @@
+namespace TelegramBotFramework.Strategies;
+
+/// <summary>
+/// Constants for RateLimitingStrategyExtensions.
+/// </summary>
+internal static class RateLimitingStrategyExtensionsConstants
+{
+}
