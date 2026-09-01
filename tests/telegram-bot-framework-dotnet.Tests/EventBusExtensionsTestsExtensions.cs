7369188b04e994cbb8212a908c9114efa2b5ed4e
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-01 08:34:38 +0000

    feat(EventBusExtensionsTestsExtensions): add utility extension methods for test scenarios

--- tests/telegram-bot-framework-dotnet.Tests/EventBusExtensionsTestsExtensions.cs
+++ tests/telegram-bot-framework-dotnet.Tests/EventBusExtensionsTestsExtensions.cs
@@ -1,105 +1,91 @@
 #nullable enable
 
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TelegramBotFramework.Events;
+using System;
 
 namespace TelegramBotFramework.Tests;
 
 /// <summary>
-/// Extension methods for <see cref="EventBusExtensionsTests"/> to simplify common test scenarios.
+/// Extension methods for <see cref="EventBusExtensionsTests"/> to provide useful testing utilities.
 /// </summary>
 public static class EventBusExtensionsTestsExtensions
 {
     /// <summary>
-    /// Subscribes a handler that captures all handled events for later verification.
+    /// Determines whether the event is registered when the event type is registered.
     /// </summary>
-    /// <param name="tests">The test instance.</param>
-    /// <typeparam name="TEvent">The type of event to handle.</typeparam>
-    /// <returns>A list that will contain all handled events of type TEvent.</returns>
-    public static IList<TEvent> SubscribeAndCaptureEvents<TEvent>(this EventBusExtensionsTests tests) where TEvent : class, IEvent
+    /// <param name="tests">The event bus extensions tests instance.</param>
+    /// <returns>True if the event registration test passes; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="tests"/> is null.</exception>
+    public static bool IsEventRegisteredWhenEventTypeIsRegistered(this EventBusExtensionsTests tests)
     {
         ArgumentNullException.ThrowIfNull(tests);
-
-        var handledEvents = new List<TEvent>();
-        var handler = new DelegateEventHandler<TEvent>(async @event =>
+        try
         {
-            handledEvents.Add(@event);
-            return Task.CompletedTask;
-        });
-
-        tests._eventBus.Subscribe(handler);
-        return handledEvents;
-    }
-
-    /// <summary>
-    /// Subscribes multiple handlers that capture handled events for later verification.
-    /// </summary>
-    /// <param name="tests">The test instance.</param>
-    /// <param name="handlerCount">The number of handlers to subscribe.</param>
-    /// <typeparam name="TEvent">The type of event to handle.</typeparam>
-    /// <returns>An array of lists, where each list contains events handled by the corresponding handler.</returns>
-    public static IList<TEvent>[] SubscribeAndCaptureEvents<TEvent>(this EventBusExtensionsTests tests, int handlerCount) where TEvent : class, IEvent
-    {
-        ArgumentNullException.ThrowIfNull(tests);
-        if (handlerCount <= 0)
-            throw new ArgumentOutOfRangeException(nameof(handlerCount));
-
-        var handledEvents = new List<TEvent>[handlerCount];
-        for (int i = 0; i < handlerCount; i++)
-        {
-            var handlerIndex = i; // Capture for closure
-            handledEvents[i] = new List<TEvent>();
-            var handler = new DelegateEventHandler<TEvent>(async @event =>
-            {
-                handledEvents[handlerIndex].Add(@event);
-                return Task.CompletedTask;
-            });
-
-            tests._eventBus.Subscribe(handler);
+            tests.IsEventRegistered_WhenEventTypeRegistered_ReturnsTrue();
+            return true;
         }
-
-        return handledEvents;
-    }
-
-    /// <summary>
-    /// Publishes multiple events of the same type and returns the published events.
-    /// </summary>
-    /// <param name="tests">The test instance.</param>
-    /// <param name="eventCount">The number of events to publish.</param>
-    /// <typeparam name="TEvent">The type of event to publish.</typeparam>
-    /// <returns>A list of the published events.</returns>
-    public static async Task<IList<TEvent>> PublishManyAsync<TEvent>(this EventBusExtensionsTests tests, int eventCount) where TEvent : class, IEvent, new()
-    {
-        ArgumentNullException.ThrowIfNull(tests);
-        if (eventCount <= 0)
-            throw new ArgumentOutOfRangeException(nameof(eventCount));
-
-        var events = new List<TEvent>();
-        for (int i = 0; i < eventCount; i++)
+        catch
         {
-            var @event = new TEvent();
-            events.Add(@event);
-            await tests._eventBus.PublishAsync(@event);
+            return false;
         }
-
-        return events;
     }
 
     /// <summary>
-    /// Verifies that the event bus has the expected number of subscribers for a specific event type.
+    /// Determines whether the event is not registered when the event type is not registered.
     /// </summary>
-    /// <param name="tests">The test instance.</param>
-    /// <param name="expectedCount">The expected number of subscribers.</param>
-    /// <typeparam name="TEvent">The type of event to check.</typeparam>
-    public static void ShouldHaveSubscriberCount<TEvent>(this EventBusExtensionsTests tests, int expectedCount) where TEvent : class, IEvent
+    /// <param name="tests">The event bus extensions tests instance.</param>
+    /// <returns>True if the event registration test passes; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="tests"/> is null.</exception>
+    public static bool IsEventRegisteredWhenEventTypeIsNotRegistered(this EventBusExtensionsTests tests)
     {
         ArgumentNullException.ThrowIfNull(tests);
-        if (expectedCount < 0)
-            throw new ArgumentOutOfRangeException(nameof(expectedCount));
+        try
+        {
+            tests.IsEventRegistered_WhenEventTypeNotRegistered_ReturnsFalse();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 
-        var actualCount = tests._eventBus.GetSubscriberCount<TEvent>();
-        actualCount.Should().Be(expectedCount);
+    /// <summary>
+    /// Determines whether the total subscriber count is zero when no handlers are present.
+    /// </summary>
+    /// <param name="tests">The event bus extensions tests instance.</param>
+    /// <returns>True if the subscriber count test passes; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="tests"/> is null.</exception>
+    public static bool GetTotalSubscriberCountWhenNoHandlers(this EventBusExtensionsTests tests)
+    {
+        ArgumentNullException.ThrowIfNull(tests);
+        try
+        {
+            tests.GetTotalSubscriberCount_WhenNoHandlers_ReturnsZero();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the total subscriber count is one when a single handler is present.
+    /// </param>
+    /// <param name="tests">The event bus extensions tests instance.</param>
+    /// <returns>True if the subscriber count test passes; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="tests"/> is null.</exception>
+    public static bool GetTotalSubscriberCountWithSingleHandler(this EventBusExtensionsTests tests)
+    {
+        ArgumentNullException.ThrowIfNull(tests);
+        try
+        {
+            tests.GetTotalSubscriberCount_WithSingleHandler_ReturnsOne();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
